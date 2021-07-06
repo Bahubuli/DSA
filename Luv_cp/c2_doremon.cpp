db56108adf3cfd1126a74ec84662463d63fee2fb
jitendra singh <33808010+Bahubuli@users.noreply.github.com>
2021-07-06 22:21:14 +0530

    ..

---
+++ Luv_cp/c2_doremon.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+const int N = 1e5+10;
+int arr[N];
+
+int main()
+{
+
+        // freopen("i.txt", "r", stdin);
+        // freopen("o.txt", "w", stdout);
+
+        int m, n;
+        cin >> m >> n;
+
+        while(m--)
+        {
+            int k, d;
+            cin >> k >> d;
+            arr[d]==1; //  = ==
+
+        }
+
+        string s = "YES";
+        while(n--)
+        {
+            int q;
+            cin >> q;
+
+            if(arr[q]==0)
+            {
+                s = "NO";
+                break;
+            }
+        }
+        cout << s ;
+}
