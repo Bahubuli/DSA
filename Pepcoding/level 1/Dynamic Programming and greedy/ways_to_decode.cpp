4a405b0d36c6d0e825d849bee694788c4246c39c
jitendra singh <jitendra.singh.sola@gmail.com>
2021-07-25 19:30:49 +0530

    ..

--- Pepcoding/level 1/Dynamic Programming and greedy/ways_to_decode.cpp
+++ Pepcoding/level 1/Dynamic Programming and greedy/ways_to_decode.cpp
@@ -1,6 +1,5 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 int main()
 {
@@ -11,9 +10,9 @@ int main()
   vector<int> dp(s.length());
   dp[0] = 1;
 
-  for(int i=1;i<dp.size();i++)
+  for (int i = 1; i < dp.size(); i++)
   {
-    if(s[i-1]=='0' && s[i]=='0')
+    if (s[i - 1] == '0' && s[i] == '0')
     {
       dp[i] = 0;
     }
@@ -23,26 +22,26 @@ int main()
     }
     else if (s[i - 1] != '0' && s[i] == '0')
     {
-      if(s[i-1]=='1' || s[i-1]=='2')
+      if (s[i - 1] == '1' || s[i - 1] == '2')
       {
-        dp[i] = dp[i - 2];
+        dp[i] = (i >= 2 ? dp[i - 2] : 1);
       }
       else
       {
         dp[i] = 0;
       }
-
     }
     else
     {
-      if(stoi(s.substr(i-1,2))<=26)
+      if (stoi(s.substr(i - 1, 2)) <= 26)
       {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] = dp[i - 1] + (i >= 2 ? dp[i - 2] : 1);
       }
       else
       {
         dp[i] = dp[i - 1];
       }
-
     }
+  }
+  cout << dp[s.length()-1] << endl;
 }
