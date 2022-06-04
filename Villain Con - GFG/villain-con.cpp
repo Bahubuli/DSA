de7d5be3757ce0e9c3457be04f75c483e5747b00
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-06-05 00:08:41 +0530

    Added solution - LeetHub

--- Villain Con - GFG/villain-con.cpp
+++ Villain Con - GFG/villain-con.cpp
@@ -10,39 +10,34 @@ using namespace std;
 class Solution{
 public:
     int minColour(int N, int M, vector<int> mat[]) {
-        vector<vector<int>>adj(N+1);
-        vector<int>indegree(N+1);
-        
-        for(int i=0;i<M;i++){
-            adj[mat[i] [0]].push_back(mat[i][1]);
-            indegree[mat[i][1]]++;
-            
-        }
-        int level = 0;
-        queue<int>q;
-        for(int i=1;i<=N;i++){
-            if(indegree[i]==0)
-            q.push(i);
-            
-        }
-        if(q.empty())
-        return 0;
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int f = q.front();
-                q.pop();
-                for(auto it : adj[f]){
-                    indegree[it]--;
-                    if(indegree[it] == 0)
-                    q.push(it);
-                    
-                }
-                
-            }
-           level++; 
-        }
-        return level;
+       vector<vector<int>> adj(N+1);
+       vector<int>ind(N+1);
+       int ans=0;
+       for(int i=0;i<M;i++)
+       {
+           adj[mat[i][0]].push_back(mat[i][1]);
+           ind[mat[i][1]]++;
+       }
+       queue<int>q;
+       for(int i=1;i<=N;i++)
+       if(ind[i]==0) q.push(i);
+       
+       while(q.size())
+       {
+           int s = q.size();
+           for(int i=0;i<s;i++)
+           {
+               int f = q.front();
+               q.pop();
+               for(auto it: adj[f])
+               {
+                   ind[it]--;
+                   if(ind[it]==0) q.push(it);
+               }
+           }
+           ans++;
+       }
+       return ans;
     }
 };
 
