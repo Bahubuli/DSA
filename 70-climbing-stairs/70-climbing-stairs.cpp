f2f7a696f6d23c2a6d229c9e7f27a9ef0c368265
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-06-08 20:57:34 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.37%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int dp[46]= {0};
-   
     int climbStairs(int n) 
     {
-        if(n<0) return 0;
-        if(n==0|| n==1) return 1;
+        int dp[46] = {0};
+        dp[0] = dp[1] = 1;
         
-       
-        
-        if(dp[n]!=0) return dp[n];
-        return dp[n] =  climbStairs(n-1)+climbStairs(n-2);
+        for(int i=2;i<=n;i++)
+            dp[i] = dp[i-1]+dp[i-2];
         
         
+        return dp[n];
         
     }
 };
