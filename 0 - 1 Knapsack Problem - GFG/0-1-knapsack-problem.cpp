49be89aeabeb88f2d301ba5402b21f910f822e08
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-06-11 07:41:19 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -17,8 +17,9 @@ class Solution
            for(int j=1;j<=w;j++)
            {
                if(wt[i-1]<=j)
-               dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
-           
+               {
+                   dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+               }
                else dp[i][j] = dp[i-1][j];
            }
        }
