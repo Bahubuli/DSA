3d4f1ca7d4b614fbb468848eccaacd965c93d270
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-06-20 18:09:26 +0530

    Added solution - LeetHub

--- Swapping pairs make sum equal - GFG/swapping-pairs-make-sum-equal.cpp
+++ Swapping pairs make sum equal - GFG/swapping-pairs-make-sum-equal.cpp
@@ -8,40 +8,40 @@ class Solution{
 	public:
 	int findSwapValues(int A[], int n, int B[], int m)
 	{
-        // long long int s1=0,s2=0,i=0,j=0;
+       int s1=0,s2=0;
         
-        // for(int i=0;i<n;i++) s1+=A[i];
-        // for(int i=0;i<m;i++) s2+=B[i];
+        for(int i=0;i<n;i++) s1+=A[i];
+        for(int i=0;i<m;i++) s2+=B[i];
         
-        // sort(A,A+n);
-        // sort(B,B+n);
-        
-        // while(i<n && j<m)
-        // {
-        //   long long  int v1 = s1 - A[i] + B[j];
-        //   long long  int v2 = s2 - B[j] + A[i];
+        sort(A,A+n);
+        sort(B,B+m);
+        int i=0,j=0;
+        while(i<n && j<m)
+        {
+          int v1 = s1 - A[i] + B[j];
+       int v2 = s2 - B[j] + A[i];
             
-        //     if(v1==v2) return 1;
+            if(v1==v2) return 1;
             
-        //     if(v1>v2) i++;
-        //     else j++;
-        // }
-        // return -1;
+            else if(v1>v2) i++;
+            else j++;
+        }
+        return -1;
         
-        int sum1=0,sum2=0;
-       for(int i=0;i<n;i++) sum1 += A[i];
-       for(int i=0;i<m;i++) sum2 += B[i];
-       sort(A,A+n);
-       sort(B,B+m);
-       int i=0,j=0;
-       while(i<n && j<m){
-           int v1 = sum1-A[i]+B[j];
-           int v2 = sum2+A[i]-B[j];
-           if(v1==v2) return 1;
-           else if(v1>v2) i++;
-           else j++;
-       }
-       return -1;
+    //     int sum1=0,sum2=0;
+    //   for(int i=0;i<n;i++) sum1 += A[i];
+    //   for(int i=0;i<m;i++) sum2 += B[i];
+    //   sort(A,A+n);
+    //   sort(B,B+m);
+    //   int i=0,j=0;
+    //   while(i<n && j<m){
+    //       int v1 = sum1-A[i]+B[j];
+    //       int v2 = sum2+A[i]-B[j];
+    //       if(v1==v2) return 1;
+    //       else if(v1>v2) i++;
+    //       else j++;
+    //   }
+    //   return -1;
 	}
  
 
