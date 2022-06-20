0053700f967a3602ce4efecb31fa3dad9986fd25
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-06-20 18:06:09 +0530

    Added solution - LeetHub

---
+++ Swapping pairs make sum equal - GFG/swapping-pairs-make-sum-equal.cpp
@@ -0,0 +1,79 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+
+	public:
+	int findSwapValues(int A[], int n, int B[], int m)
+	{
+        // long long int s1=0,s2=0,i=0,j=0;
+        
+        // for(int i=0;i<n;i++) s1+=A[i];
+        // for(int i=0;i<m;i++) s2+=B[i];
+        
+        // sort(A,A+n);
+        // sort(B,B+n);
+        
+        // while(i<n && j<m)
+        // {
+        //   long long  int v1 = s1 - A[i] + B[j];
+        //   long long  int v2 = s2 - B[j] + A[i];
+            
+        //     if(v1==v2) return 1;
+            
+        //     if(v1>v2) i++;
+        //     else j++;
+        // }
+        // return -1;
+        
+        int sum1=0,sum2=0;
+       for(int i=0;i<n;i++) sum1 += A[i];
+       for(int i=0;i<m;i++) sum2 += B[i];
+       sort(A,A+n);
+       sort(B,B+m);
+       int i=0,j=0;
+       while(i<n && j<m){
+           if(sum1-A[i]+B[j]==sum2+A[i]-B[j]) return 1;
+           else if(sum1-A[i]+B[j]>sum2+A[i]-B[j]) i++;
+           else j++;
+       }
+       return -1;
+	}
+ 
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,m;
+        cin>>n>>m;
+        int a[n];
+        int b[m];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        for(int i=0;i<m;i++)
+            cin>>b[i];
+        
+
+        Solution ob;
+        cout <<  ob.findSwapValues(a, n, b, m);
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+  // } Driver Code Ends
