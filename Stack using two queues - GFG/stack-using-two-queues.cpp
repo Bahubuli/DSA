3dae86645ec55dd8b6158d88993561b847ab5dd4
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-06-22 09:24:52 +0530

    Added solution - LeetHub

--- Stack using two queues - GFG/stack-using-two-queues.cpp
+++ Stack using two queues - GFG/stack-using-two-queues.cpp
@@ -41,34 +41,30 @@ int main()
 // } Driver Code Ends
 
 
-/* The structure of the class is
-class QueueStack{
-private:
-    queue<int> q1;
-    queue<int> q2;
-public:
-    void push(int);
-    int pop();
-};
- */
 
 //Function to push an element into stack using two queues.
 void QueueStack :: push(int x)
 {
-        int s = q1.size();
-        q1.push(x);
-        for(int i=0;i<s;i++)
-        {
-            q1.push(q1.front());
-            q1.pop();
-        }
+       q1.push(x);
 }
 
 //Function to pop an element from stack using two queues. 
 int QueueStack :: pop()
 {
-    if(q1.size()==0) return -1;
-        int ans = q1.front();
+    if(q1.empty()) return -1;
+    
+    while(q1.size()>1)
+    {
+        q2.push(q1.front());
         q1.pop();
-        return ans;
+    }
+    int x = q1.front();
+    q1.pop();
+    
+    while(q2.size())
+    {
+        q1.push(q2.front());
+        q2.pop();
+    }
+    return x;
 }
