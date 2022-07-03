c8af3ed203cff8e3872269015a77e3f4bb78943a
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-07-03 09:36:51 +0530

    Time: 306 ms (66.67%), Space: 127.8 MB (100.00%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int n, int m, ListNode* head) 
+    {
+        vector<int> nums;
+        ListNode *temp = head;
+        
+        
+        vector<vector<int>>mat(n,vector<int>(m,-1));
+        
+        int u=0,l=0,d=n-1,r =m-1,p=0;
+        
+        while(l<=r && u<=d)
+        {
+            for(int col = l;col<=r;col++)
+               if(head)
+               {
+                   mat[u][col] = head->val;
+                   head = head->next;
+               }
+            
+            if(++u>d) break;
+            for(int row = u;row<=d;row++)
+            {
+               if(head)
+               {mat[row][r] = head->val;
+                head = head->next;
+               }
+            }
+            
+            if(--r<l) break;
+            
+            for(int col=r;col>=l;col--)
+            {
+               if(head)
+               {mat[d][col]=head->val;
+                     head = head->next;
+               }
+            }
+            
+            if(--d<u) break;
+            
+            for(int row=d;row>=u;row--)
+            {
+               if(head)
+               {
+                   mat[row][l] = head->val;
+                    head = head->next;
+               }
+                
+            }
+            
+            if(++l>r) break;
+        }
+        return mat;
+        
+        
+    }
+};
