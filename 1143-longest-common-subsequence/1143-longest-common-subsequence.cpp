d2b8835c4da4a6fde70d513190b11c7dbe5bbf7d
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-07-15 10:37:19 +0530

    Time: 50 ms (25.34%), Space: 12.9 MB (53.25%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     
-    int lcs(string &s1,string &s2,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i>=s1.size() || j>=s2.size()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int match = s1[i]==s2[j];
-        if(match) match+=lcs(s1,s2,i+1,j+1,dp);
-        
-        int nmatch = max(lcs(s1,s2,i,j+1,dp),lcs(s1,s2,i+1,j,dp));
-        
-        return dp[i][j] = max(match,nmatch);
-    }
+   
     
     int longestCommonSubsequence(string s1, string s2) 
     {
         
         int n = s1.size(),m =s2.size();
+        if(m>n) return longestCommonSubsequence(s2,s1);
         vector<vector<int>>dp(n,vector<int>(m,0));
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++) dp[0][i] = (s1[0]==s2[i]);
+        
+        for(int i=1;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 int match =(s1[i]==s2[j]),nmatch = 0;
-                if(match && j>0 && i>0) match+= dp[i-1][j-1];
+                if(match && j>0) match+= dp[i-1][j-1];
                 
-                if(i>0) nmatch = max(nmatch,dp[i-1][j]);
+                nmatch = max(nmatch,dp[i-1][j]);
                 if(j>0) nmatch = max(nmatch,dp[i][j-1]);
                 
                 dp[i][j] = max(match,nmatch);
             }
         }
         
-       
+        //for(int i=0;i<m;i++) cout<<dp[0][i]<<endl;
        return dp[n-1][m-1];
         
     }
