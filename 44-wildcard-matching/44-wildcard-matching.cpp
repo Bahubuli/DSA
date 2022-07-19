45fea8ebb88df2cf5d29f1fed385a82e716d5a5f
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-07-19 17:12:03 +0530

    Time: 132 ms (28.00%), Space: 11.3 MB (61.85%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -4,26 +4,26 @@ public:
     {
    int n = s1.size(),m=s2.size();
    vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
-    dp[0][0] = true;  
+    dp[n][m] = true;  
     
         
-    for(int j=0;j<m && s2[j]=='*';j++)
+    for(int j=m-1;j>=0 && s2[j]=='*';j--)
     {
-        dp[0][j+1] = true;
+       dp[n][j] = true;
     } 
         
-
-    for(int i=1;i<=n;i++)
+    //cout<<dp[2][1]<<endl;
+    for(int i=n-1;i>=0;i--)
     {
-        for(int j=1;j<=m;j++)
+        for(int j=m-1;j>=0;j--)
         {
-            if(s2[j-1]=='*') dp[i][j] = dp[i-1][j]+dp[i][j-1];
-            else if(s2[j-1]=='?' || s1[i-1]==s2[j-1]) dp[i][j] = dp[i-1][j-1];
+            if(s2[j]=='*') dp[i][j] = dp[i+1][j]+dp[i][j+1];
+            else if(s2[j]=='?' || s1[i]==s2[j]) dp[i][j] = dp[i+1][j+1];
             
         }
     }
 
-      return dp[n][m];  
+      return dp[0][0];  
         
     }
 };
