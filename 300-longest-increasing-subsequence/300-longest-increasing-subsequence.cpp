04af0d2a8b27d10b20d2d2d7de9e6525ca41faa1
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-07-25 08:20:43 +0530

    Time: 704 ms (16.48%), Space: 10.7 MB (24.23%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     
-    int helper(vector<int>&nums,int i,int pi,  vector<vector<int>>&dp)
+    int helper(vector<int>&nums,int i,int pi,  vector<int>&dp)
     {
         int n = nums.size(),pick=0,npick=0;
         if(i==n) return 0;
-        if(dp[i][pi+1]!=-1) return dp[i][pi+1];
+        
+        if(dp[pi+1]!=-1) return dp[pi+1];
         
         if(pi==-1 || nums[pi]<nums[i]) pick = 1+helper(nums,i+1,i,dp);
         npick = helper(nums,i+1,pi,dp);
         
-        return dp[i][pi+1] =  max(pick,npick);
+        return dp[pi+1] =  max(pick,npick);
     }
     int lengthOfLIS(vector<int>& nums) 
     {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        vector<int>dp(n+1,-1);
         return helper(nums,0,-1,dp);
         
     }
