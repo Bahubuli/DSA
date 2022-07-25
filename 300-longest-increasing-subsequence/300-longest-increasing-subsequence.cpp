a236f2b888b1946b52ac9e8f5d764b9dec1fb1cd
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-07-25 10:21:59 +0530

    Time: 8 ms (94.83%), Space: 10.4 MB (87.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
-    
-    int helper(vector<int>&nums,int i,int pi,vector<vector<int>>&dp)
-    {
-        int n = nums.size(),pick=0,npick=0;
-        if(i==n) return 0;
-        if(dp[i][pi+1]!=-1) return dp[i][pi+1];
-        
-        if(pi==-1 || nums[pi]<nums[i]) pick = 1+helper(nums,i+1,i,dp);
-        npick = helper(nums,i+1,pi,dp);
-        
-        return dp[i][pi+1] =  max(pick,npick);
-    }
     int lengthOfLIS(vector<int>& nums) 
     {
+        
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+2,0));
+        vector<int>res;
+        res.push_back(nums[0]);
         
-       
-        
-        for(int i=n-1;i>=0;i--)
+        for(int i=1;i<n;i++)
         {
-            
-            for(int pi=i-1;pi>=-1;pi--)
+            if(nums[i]>res.back()) res.push_back(nums[i]);
+            else
             {
-                int pick=0,npick=0;
-                npick = dp[i+1][pi+1];
-                
-                if(pi==-1 || nums[pi]<nums[i])
-                    pick = 1+dp[i+1][i+1];
-                    
-                dp[i][pi+1] = max(pick,npick);
+               *lower_bound(res.begin(),res.end(),nums[i]) = nums[i];
+             
             }
         }
-        
-        
-        
-        return dp[0][0];
+       
+        return res.size();
         
     }
 };
