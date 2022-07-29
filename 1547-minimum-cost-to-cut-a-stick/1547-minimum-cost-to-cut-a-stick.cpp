954631ef930bbd70d2a6894ef3d7ce0737dfb83e
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-07-29 09:29:25 +0530

    Time: 132 ms (36.83%), Space: 10.5 MB (11.93%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -3,18 +3,16 @@ public:
     
     int helper(vector<int>&cuts,int i,int j, vector<vector<int>>&dp)
     {
+        int res = 1e9;
         
         if(dp[i][j]!=-1) return dp[i][j];
-        int res = 1e9;
-        for(int k = i;k<=j;k++)
-        {
-            res = min(res,cuts[j+1]-cuts[i-1] +
-                      helper(cuts,i,k-1,dp)+helper(cuts,k+1,j,dp));
-        }
         
-        if(res==1e9) return dp[i][j] = 0;
-        return dp[i][j] = res;
+        for(int k=i;k<=j;k++)
+            res = min(res,cuts[j+1]-cuts[i-1]+helper(cuts,i,k-1,dp)+helper(cuts,k+1,j,dp));
+        
+        return dp[i][j] = res==1e9 ? 0 : res;
     }
+    
     
     int minCost(int n, vector<int>& cuts) 
     {
