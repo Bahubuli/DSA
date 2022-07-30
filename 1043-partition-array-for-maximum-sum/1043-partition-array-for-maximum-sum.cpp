7a72c3789499697057ff3139c83806fc2b7c4b3a
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-07-30 11:43:23 +0530

    Time: 18 ms (82.18%), Space: 8.3 MB (91.96%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     
-    int dp[501][501];
+    int dp[501];
     
     int helper(vector<int>&nums,int i,int j,int k)
     {
         if(i>j) return 0;
         if(i==j) return nums[i];
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i]!=-1) return dp[i];
         
         int ans = 0;
         int mx =0;
@@ -18,35 +18,39 @@ public:
             ans = max(ans,(d-i+1)*mx+helper(nums,d+1,j,k));
         }
         
-        return dp[i][j] =  ans;
+        return dp[i] =  ans;
     }
     
     int maxSumAfterPartitioning(vector<int>& nums, int k) 
     {
         int n = nums.size();
-        memset(dp,0,sizeof(dp));
+         memset(dp,-1,sizeof(dp));
+         return helper(nums,0,n-1,k);
         
-        for(int i=0;i<n;i++) dp[i][i] = nums[i];
         
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(j<=i) continue;
+//         memset(dp,0,sizeof(dp));
+        
+//         for(int i=0;i<n;i++) dp[i][i] = nums[i];
+        
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(j<=i) continue;
                 
-                  int ans = 0;
-                  int mx =0;
-                  for(int d=i;d<min(i+k,j+1);d++)
-                  {
+//                   int ans = 0;
+//                   int mx =0;
+//                   for(int d=i;d<min(i+k,j+1);d++)
+//                   {
 
-                      mx = max(mx,nums[d]);
-                      ans = max(ans,(d-i+1)*mx+ dp[d+1][j]);
-                  }
+//                       mx = max(mx,nums[d]);
+//                       ans = max(ans,(d-i+1)*mx+ dp[d+1][j]);
+//                   }
 
-                   dp[i][j] =  ans;
-            }
-        }
+//                    dp[i][j] =  ans;
+//             }
+//         }
        
-        return dp[0][n-1];
-    }
+//         return dp[0][n-1];
+   }
 };
