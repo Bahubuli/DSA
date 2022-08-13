d1ce9ac1efa2cec1bb52a1ec851b9f0a8292d67f
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-08-13 17:03:12 +0530

    Time: 256 ms (73.36%), Space: 37.5 MB (44.12%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-     vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> cnt; // Expected times of each word
-        for(auto &w : words) cnt[w]++;
+    vector<int> findSubstring(string s, vector<string>& words) 
+    {
+        unordered_map<string,int> mp;
+        for(auto &w:words) mp[w]++;
+        vector<int> ans;
         int n = words.size();
         int k = words[0].size();
-        vector<int> ans;
-         
+        
         for(int i=0;i+k*n<=s.size();i++)
         {
-            unordered_map<string,int> seen;
+            unordered_map<string,int> freq;
             
             for(int j=0;j<n;j++)
             {
-                string str = s.substr(i+j*k,k);
-                seen[str]++;
+                string res = s.substr(i+j*k,k);
+                freq[res]++;
+                if(freq[res]>mp[res]) break;
                 
-                if(seen[str]> cnt[str]) break;
                 if(j==n-1) ans.push_back(i);
             }
         }
-         
-         return ans;  
+        return ans;
     }
 };
