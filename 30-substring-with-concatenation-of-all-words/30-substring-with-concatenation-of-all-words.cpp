5d49fb9cc33ba5b8a62988ec86907a28cdbd2b81
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-08-13 16:48:42 +0530

    Time: 542 ms (42.63%), Space: 37.7 MB (43.42%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -5,16 +5,23 @@ public:
         for(auto &w : words) cnt[w]++;
         int n = words.size();
         int k = words[0].size();
-        vector<int> res;
-        for(int i = 0; i + n * k <= s.size(); i++){
-            unordered_map<string, int> seen; // Visited times of each word during the loop starting from i
-            for(int j = 0; j < n; j++){
-                string str = s.substr(i + j * k, k); 
+        vector<int> ans;
+         
+        for(int i=0;i+k*n<=s.size();i++)
+        {
+            unordered_map<string,int> seen;
+            
+            for(int j=0;j<n;j++)
+            {
+                string str = s.substr(i+j*k,k);
                 seen[str]++;
-                if(seen[str] > cnt[str]) break; // If for any words, visited times > expected times, then we stop the check.  
-                if(j == n - 1) res.push_back(i);  // If all pass, we know i is one of the valid start index.
+                
+                if(seen[str]> cnt[str]) break;
+                if(j==n-1) ans.push_back(i);
             }
         }
-        return res;
+         
+         return ans;
+        
     }
 };
