c6af5d9e6fed95c7a60d088783e610b5ec4d1175
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-08-24 09:18:56 +0530

    Time: 63 ms (5.42%), Space: 6.3 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    bool isPowerOfThree(int n) 
-    {
-        
-        for(int i=0;i<32;i++)
-        {
-            long long int p3 = pow(3,i);
-            if(p3==n) return true;
-        }
-        return false;
-        
-    }
+    bool isPowerOfThree(int n) {
+	if(n < 1) return false;
+	string nbase3 = "";
+	while(n) nbase3 += to_string(n % 3), n /= 3;   // conversion to base 3
+	int i = 0;
+	while(i < size(nbase3) - 1) if(nbase3[i++] != '0') return false; // checking if all digits in base 3 converted number except first one are 0
+	return nbase3[i] == '1';   // check if starting digit is 1
+}
 };
