1228bcbcb6ba7ad8324f0e1ecc4e69508f98aece
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-08-28 00:02:45 +0530

    Time: 1071 ms (87.20%), Space: 10.4 MB (99.22%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    int maxSumSubmatrix(vector<vector<int>>& m, int k) {
-    int res = INT_MIN, rows = m.size(), cols = m[0].size();
-    for (int l = 0; l < cols; ++l) {
-        vector<int> sums(rows);
-        for (int r = l; r < cols; ++r) {
-            for (int i = 0; i < rows; ++i) 
-                sums[i] += m[i][r];
-            set<int> s = {0};
-            int run_sum = 0;
-            for (int sum : sums) {
-                run_sum += sum;
-                auto it = s.lower_bound(run_sum - k);
-                if (it != end(s))
-                    res = max(res, run_sum - *it);
-                s.insert(run_sum);
+    int maxSumSubmatrix(vector<vector<int>>& mat, int k) 
+    {
+        int r = mat.size(), c = mat[0].size();
+        
+        int prefix[r+1][c+1];
+        memset(prefix,0,sizeof(prefix));
+        
+        for(int i=1;i<=r;i++) prefix[i][1] = prefix[i-1][1]+mat[i-1][0];
+        for(int i=1;i<=c;i++) prefix[1][i] = prefix[1][i-1]+mat[0][i-1];
+        
+        for(int i=2;i<=r;i++)
+        {
+            for(int j=2;j<=c;j++)
+            {
+                prefix[i][j] = prefix[i-1][j]+prefix[i][j-1]
+                    -prefix[i-1][j-1]+mat[i-1][j-1];
             }
         }
+        
+        int ans = INT_MIN;
+        
+        for(int x1 = 1;x1<=r;x1++)
+            for(int y1=1;y1<=c;y1++)
+                for(int x2 = x1;x2<=r;x2++)
+                    for(int y2 = y1;y2<=c;y2++)
+                    {
+                        int sum = prefix[x2][y2]-prefix[x1-1][y2]-prefix[x2][y1-1]+prefix[x1-1][y1-1];
+                        if(sum<=k) ans = max(ans,sum);
+                    }
+        
+        return ans;
     }
-    return res;
-}
 };
