0af0af5a31e89915f04d0bf9557aaddbd5bdc879
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-09-14 07:16:36 +0530

    Time: 1305 ms (11.39%), Space: 355.4 MB (14.09%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    
+    void dfs(TreeNode *root,vector<int>mp,int &ans)
+    {
+        if(!root) return;
+        
+        mp[root->val]++;
+        if(!root->left && !root->right)
+        {
+            int odd=0;
+            for(int i=1;i<=9;i++)
+            {
+                //cout<<it.first<<" "<<it.second<<endl;
+                if(mp[i]%2) odd++;
+            }
+            //cout<<odd<<endl;
+            if(odd<=1) ans++;
+            return;
+        }
+        
+        
+        dfs(root->left,mp,ans);
+        dfs(root->right,mp,ans);
+        mp[root->val]--;
+        
+    }
+    
+    int pseudoPalindromicPaths (TreeNode* root) 
+    {
+       vector<int>mp(10,0);
+        int ans = 0;
+        dfs(root,mp,ans);
+        return ans;
+        
+    }
+};
