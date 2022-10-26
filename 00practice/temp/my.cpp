c0c37ef103d3b353cbe40804b23174a8cf78a21d
Bahubali <jitendra.singh.sola@gmail.com>
2022-10-26 20:48:52 +0530

    folder structure changes

--- 00practice/temp/my.cpp
+++ 00practice/temp/my.cpp
@@ -1,51 +1,155 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void PrefixToInfix(stack<string> &stk, string prefix)
-{
-    int n = prefix.size() - 1;
-
-    // we will traverse the given prefix expression from right to left
-    for (int i = n; i >= 0; i--)
-    {
-
-        string currentVal = prefix.substr(i,1);
+// // int maxLength(string s, int n)
+// // {
 
 
-        //cout<<currentVal<<endl;
+// //         // Finding the r[] and l[] arrays.
+// //         for (int j = 0; j < n; j++) {
+// //             L.push_back((2 * freq[j - 1]) - j);
+// //             R.push_back((2 * freq[j]) - j);
+// //         }
 
-        // if an operator is encountered
-        // then pop out the last inserted two elements
-        // and concatenate them along with the operator & brackets
-        // and push the resultant back into the stack
-        if (currentVal == "+" || currentVal == "-" || currentVal == "/" || currentVal == "*" || currentVal == "^" || currentVal == "%")
-        {
-            string first = stk.top();
-            stk.pop();
-            string second = stk.top();
-            stk.pop();
+// //         int max_len = INT_MIN;
+// //         int min_val = INT_MAX;
 
-            string newVal = "(" + first + currentVal + second + ")";
-            stk.push(newVal);
-        }
+// //         // for each j from 0 to n
+// //         for (int j = 0; j < n; j++) {
+// //             min_val = min(min_val, L[j]);
+// //             A.push_back(min_val);
 
-        else
-            // if we'll encounter any operand then we'll simply push it into the stack
-            stk.push(currentVal);
-    }
-}
+// //             int l = 0, r = j;
 
+// //             // Finding the lower bound of i.
+// //             while (l <= r) {
+// //                 int mid = (l + r) >> 1;
+// //                 if (A[mid] <= R[j]) {
+// //                     max_len = max(max_len, j - mid + 1);
+// //                     r = mid - 1;
+// //                 }
+// //                 else {
+// //                     l = mid + 1;
+// //                 }
+// //             }
+// //         }
+
+// //         // storing the maximum value of i - j + 1
+// //         ans = max(ans, max_len);
+
+// //         // clearing all the vector so that it clearing
+// //         // be use for other character.
+// //         A.clear();
+// //         R.clear();
+// //         L.clear();
+// //     }
+
+// //     return ans;
+// // }
+
+// string mins(string s,int n)
+// {
+//     int ans = INT_MIN;
+//     vector<int> A, L, R;
+//     int freq[n + 5];
+
+//     for (int i = 0; i < 26; i++)
+//     {
+//         int count = 0;
+//         memset(freq, 0, sizeof freq);
+
+
+//         for (int j = 0; j < n; j++)
+//         {
+//             if (s[j] - 'a' == i)
+//                 count++;
+//             freq[j] = count;
+//         }
+//     }
+
+// }
+
+
+// int main()
+// {
+
+
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // Function to return the length of the longest
+// // sub string having frequency of a character
+// // greater than half of the length of the
+// // sub string
+// int maxLength(string s, int n)
+// {
+//     int ans = INT_MAX;
+//     vector<int> A, L, R;
+//     int freq[n + 5];
+
+//     // for each of the character 'a' to 'z'
+//     for (int i = 0; i < 26; i++) {
+//         int count = 0;
+//         memset(freq, 0, sizeof freq);
+
+//         // finding frequency prefix array of the
+//         // character
+//         for (int j = 0; j < n; j++) {
+//             if (s[j] - 'a' == i)
+//                 count++;
+//             freq[j] = count;
+//         }
+
+//         // Finding the r[] and l[] arrays.
+//         for (int j = 0; j < n; j++) {
+//             L.push_back((2 * freq[j - 1]) - j);
+//             R.push_back((2 * freq[j]) - j);
+//         }
+
+//         int max_len = INT_MAX;
+//         int min_val = INT_MIN;
+
+//         // for each j from 0 to n
+//         for (int j = 0; j < n; j++) {
+//             min_val = min(min_val, L[j]);
+//             A.push_back(min_val);
+
+//             int l = 0, r = j;
+
+//             // Finding the lower bound of i.
+//             while (l <= r) {
+//                 int mid = (l + r) >> 1;
+//                 if (A[mid] < R[j]) {
+//                     if(r-mid>=1)
+//                     max_len = min(max_len, j - mid + 1);
+//                     r = mid - 1;
+//                 }
+//                 else {
+//                     l = mid + 1;
+//                 }
+//             }
+//         }
+
+//         // storing the maximum value of i - j + 1
+//         ans = min(ans, max_len);
+
+//         // clearing all the vector so that it clearing
+//         // be use for other character.
+//         A.clear();
+//         R.clear();
+//         L.clear();
+//     }
+
+//     return ans;
+// }
+
+// Driver Code
 int main()
 {
-    string prefix;
-    cout << "Enter a valid prefix expression : ";
-    cin >> prefix;
+    bool res = "abb"<"bb";
+    cout<<res<<endl;
 
-    stack<string> stk;
-
-    PrefixToInfix(stk, prefix);
-
-    cout << "The infix expression of the given prefix expression is : " << stk.top();
-
-    return 0;
 }
