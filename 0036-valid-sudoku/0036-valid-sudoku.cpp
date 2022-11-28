2058b856cb2c91c694813866f23949a22f51a20a
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-11-28 21:12:10 +0530

    Time: 55 ms (29.91%), Space: 18 MB (87.50%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-                int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+        int mprow[9][9]={0}, mpcol[9][9]={0}, mp3x3[9][9]={0};
         
-        for(int i = 0; i < board.size(); ++ i)
-            for(int j = 0; j < board[i].size(); ++ j)
-                if(board[i][j] != '.')
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[i].size();j++)
+            {
+                if(board[i][j]!='.')
                 {
-                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
-                    if(used1[i][num] || used2[j][num] || used3[k][num])
+                    int num = board[i][j]-'0'-1;
+                    
+                    int k = i/3*3+j/3;
+                    if(mprow[i][num] || mpcol[j][num] || mp3x3[k][num])
                         return false;
-                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
+                    
+                    mprow[i][num] = mpcol[j][num] = mp3x3[k][num] = 1;
                 }
-        
+            }
+        }
         return true;
-
     }
 };
