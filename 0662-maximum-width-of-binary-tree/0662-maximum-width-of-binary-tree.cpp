982bcfd0988f2a889f02498d1e9573654ffe8164
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-11-30 00:20:12 +0530

    Time: 11 ms (84.78%), Space: 17.2 MB (90.16%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -11,47 +11,31 @@
  */
 class Solution {
 public:
-    
-      int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL)
-            return 0;
+    int widthOfBinaryTree(TreeNode* root) 
+    {
+        if(!root) return 0;
         
-        int res = 1;
-        queue<pair<TreeNode*, int>> q;
+        int ans = 1;
+        queue<pair<TreeNode *,long long>>q;
+        q.push({root,0});
         
-        // I am using intialising list
-        q.push({root, 0});      // also can use make_pair
-        
-        while(!q.empty())
+        while(q.size())
         {
-            int cnt = q.size();
-            // start is the index of root node for first level
+            int s= q.size();
             int start = q.front().second;
             int end = q.back().second;
+            ans = max(ans,end-start+1);
             
-            res = max(res,end-start + 1);
-            
-            for(int i = 0; i <cnt; ++i)
+            while(s--)
             {
-                pair<TreeNode*, int> p = q.front();
-                // we will use it while inserting it children
-                // left child will be 2 * idx + 1;
-                // right chils will be 2 * idx + 2;
-                int idx = p.second - start;
-                
+                auto node = q.front().first;
+                int idx = q.front().second-start;
                 q.pop();
                 
-                // if  left child exist
-                if(p.first->left != NULL)
-                    q.push({p.first->left, (long long)2 * idx + 1});
-                
-                // if right child exist
-                if(p.first->right != NULL)
-                    q.push({p.first->right, (long long) 2 * idx + 2});
+                if(node->left) q.push({node->left,(long long)2*idx+1});
+                if(node->right) q.push({node->right,(long long)2*idx+2});
             }
         }
-        
-        return res;
-        
+        return ans;
     }
 };
