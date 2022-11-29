11bd165c61365b7a8ad9cd6b3fcf83104efa0785
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-11-29 23:27:44 +0530

    Time: 4 ms (98.72%), Space: 17.4 MB (62.94%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,22 +12,46 @@
 class Solution {
 public:
     
-    void dfs(TreeNode *root,long long int idx,int d,vector<long long int> &left,int &ans)
-    {
-         if(!root) return;
-         if(d>=left.size()) left.push_back(idx); 
-         
-        ans = max((long long)ans,idx-left[d]+1);
+      int widthOfBinaryTree(TreeNode* root) {
+        if(root == NULL)
+            return 0;
         
-        if(root->left) dfs(root->left,2*(idx-left[d]),d+1,left,ans);
-        if(root->right) dfs(root->right,2*(idx-left[d])+1,d+1,left,ans);
-    }
-    
-    int widthOfBinaryTree(TreeNode* root) 
-    {
-        int ans=1;
-        vector<long long int>left;
-        dfs(root,1,0,left,ans);
-        return ans;  
+        int res = 1;
+        queue<pair<TreeNode*, int>> q;
+        
+        // I am using intialising list
+        q.push({root, 0});      // also can use make_pair
+        
+        while(!q.empty())
+        {
+            int cnt = q.size();
+            // start is the index of root node for first level
+            int start = q.front().second;
+            int end = q.back().second;
+            
+            res = max(res,end-start + 1);
+            
+            for(int i = 0; i <cnt; ++i)
+            {
+                pair<TreeNode*, int> p = q.front();
+                // we will use it while inserting it children
+                // left child will be 2 * idx + 1;
+                // right chils will be 2 * idx + 2;
+                int idx = p.second - start;
+                
+                q.pop();
+                
+                // if  left child exist
+                if(p.first->left != NULL)
+                    q.push({p.first->left, (long long)2 * idx + 1});
+                
+                // if right child exist
+                if(p.first->right != NULL)
+                    q.push({p.first->right, (long long) 2 * idx + 2});
+            }
+        }
+        
+        return res;
+        
     }
 };
