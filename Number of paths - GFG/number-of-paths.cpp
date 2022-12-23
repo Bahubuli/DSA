317698ec97bee72c03fd8d798add3af95fdccae6
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2022-12-23 15:44:58 +0530

    Added solution - LeetHub

--- Number of paths - GFG/number-of-paths.cpp
+++ Number of paths - GFG/number-of-paths.cpp
@@ -6,18 +6,22 @@ using namespace std;
 // } Driver Code Ends
 
 
-long long int helper(int i,int j,int m,int n)
+long long int helper(int i,int j,int m,int n,int dp[11][11])
 {
     if(i==m || j==n) return 0;
     
     if(i==m-1 && j==n-1) return 1;
     
-    return helper(i+1,j,m,n)+helper(i,j+1,m,n);
+    if(dp[i][j]!=-1) return dp[i][j];
+    
+    return dp[i][j] = helper(i+1,j,m,n,dp)+helper(i,j+1,m,n,dp);
 }
 
 long long  numberOfPaths(int m, int n)
 {
-    return helper(0,0,m,n);
+    int dp[11][11];
+    memset(dp,-1,sizeof(dp));
+    return helper(0,0,m,n,dp);
 }
 
 //{ Driver Code Starts.
