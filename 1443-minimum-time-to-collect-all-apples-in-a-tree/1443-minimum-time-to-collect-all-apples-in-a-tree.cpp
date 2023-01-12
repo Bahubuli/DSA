ffb29bffc28138dfdbcd004ad5fe93ea4d5f9c65
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2023-01-13 00:13:21 +0530

    Time: 209 ms (70.86%), Space: 64.7 MB (47.02%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-     vector<vector<int>> adjList;
-    int dfs(vector<bool>& hasApple,int node,int d,int prev)
-    {
-        int result=0,temp;
-        for(int &i:adjList[node])
-	    if(i!=prev)
-	    {
-	        temp=dfs(hasApple,i,d+1,node);
-	        if(temp)			//If child has apples it'll return a non zero result which is the distance traveled upto that node.
-		    result+=temp-d;
-	    }
-        return result||hasApple[node]?result+d:0;  //If nothing is added to result and current node doesnt have apple return 0 else return distances of children + current distance from root.
+   
+    void createGraph(vector<vector<int>>&edges,vector<int>adj[])
+        {
+            for(auto e:edges)
+            {
+                adj[e[0]].push_back(e[1]);
+                adj[e[1]].push_back(e[0]);
+            }
+        }
         
+    
+    int dfs(int node,int ans,vector<bool>&vis,vector<bool>&apple,vector<int>adj[])
+    {
+        if(vis[node]) return 0;
+        vis[node] = true;
+        
+        int res = 0;
+        
+        for(auto adjnode:adj[node])
+            res+= dfs(adjnode,2,vis,apple,adj);
+        
+        if(res==0 && !apple[node]) return 0;
+        
+        return ans+res;
     }
+    
+    
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) 
     {
-        adjList.resize(n);
-        for(vector<int> &e:edges)
-            adjList[e[0]].push_back(e[1]),adjList[e[1]].push_back(e[0]);
-        return dfs(hasApple,0,0,-1)*2;     //Result is doubled the distance travelled as per our observation.
+        vector<int>adj[n];
+        vector<bool>vis(n,0);
+        createGraph(edges,adj);
+        return dfs(0,0,vis,hasApple,adj);
     }
 };
