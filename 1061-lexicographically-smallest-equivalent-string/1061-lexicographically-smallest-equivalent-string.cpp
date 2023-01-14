56f2049d63e2f3015f43c88f9bc3767fdfdab81e
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2023-01-15 00:28:55 +0530

    Time: 2 ms (84.40%), Space: 6.5 MB (78.90%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int par[26];
     
+    int par[26];
     int find(int x)
     {
         if(par[x]==-1) return x;
         return par[x] = find(par[x]);
     }
     
-    void Union(int x,int y)
+    void Union(int a,int b)
     {
-        x = find(x);
-        y = find(y);
+        int pa = find(a);
+        int pb = find(b);
         
-        if(x!=y) par[max(x,y)] = min(x,y);
+        if(pa!=pb)    par[max(pa,pb)] = min(pa,pb);
     }
     
-    string smallestEquivalentString(string s1, string s2, string baseStr)
+    
+    string smallestEquivalentString(string s1, string s2, string s) 
     {
         memset(par,-1,sizeof(par));
-        
-        for(int i=0;i<s1.size();i++) Union(s1[i]-'a',s2[i]-'a');
-        for(int i=0;i<baseStr.size();i++)
-            baseStr[i] = find(baseStr[i]-'a')+'a';
-        
-        return baseStr;
+        int n = s1.size();
+        for(int i=0;i<n;i++) Union(s1[i]-'a',s2[i]-'a');
+        for(int i=0;i<s.size();i++) s[i] = find(s[i]-'a')+'a';
+        return s;
         
     }
 };
