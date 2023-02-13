ed05e179bd0146c2173c8a5748758f1516a81076
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2023-02-13 11:11:20 +0530

    Time: 7 ms (65.00%), Space: 6.1 MB (75.71%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.c
+++ 0031-next-permutation/0031-next-permutation.c
@@ -1,4 +1,3 @@
-
 void swap(int *a,int *b)
 {
     int t = *a;
@@ -8,34 +7,39 @@ void swap(int *a,int *b)
 
 void reverse(int *nums,int i,int j)
 {
-    while(i<=j)
+    while(i<j)
     {
         swap(&nums[i],&nums[j]);
-            i++;
+        i++;
         j--;
     }
 }
-void nextPermutation(int* nums, int n){
-    
+void nextPermutation(int* nums, int n)
+{
     int i;
-    
     for(i=n-2;i>=0;i--)
-        if(nums[i]<nums[i+1])
-            break;
-    
-    if(i<0) reverse(nums,0,n-1);
-    else
     {
-        for(int j=n-1;j>i;j--)
-            if(nums[j]>nums[i])
-            {
-                swap(&nums[i],&nums[j]);
-                break;
-            }
-        
-        reverse(nums,i+1,n-1);
+        if(nums[i]<nums[i+1])
+        {
+           
+            break;
+        }
     }
     
-     
+    if(i<0)
+    {
+        reverse(nums,0,n-1);
+        return;
+    }
+    
+    for(int j=n-1;j>i;j--)
+    {
+        if(nums[j]>nums[i])
+        {
+            swap(&nums[i],&nums[j]);
+            reverse(nums,i+1,n-1);
+            return;
+        }
+    }
 
 }
