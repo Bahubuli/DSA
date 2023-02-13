2d7ba3c4a35f4a09de9f2bfce621649d52661dd9
Bahubali <33808010+Bahubuli@users.noreply.github.com>
2023-02-13 11:45:44 +0530

    Time: 164 ms (83.33%), Space: 13.7 MB (22.22%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.c
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.c
@@ -10,27 +10,19 @@ int min(int a,int b)
 
 int partitionDisjoint(int* nums, int n)
 {
-    int left[n], right[n] ;
-    
-    left[0] = nums[0];
-    right[n-1] = nums[n-1];
-    for(int i=1;i<n;i++)
+    int ans = 0;
+    int leftmx = nums[0];
+    int currmx = nums[0];
+    for(int i=0;i<n;i++)
     {
-        left[i] = max(left[i-1],nums[i]);
+        currmx = max(currmx,nums[i]);
+        
+        if(nums[i]<leftmx)
+        {
+            leftmx = max(currmx,leftmx);
+            ans = i;
+        }
+        
     }
-    
-     for(int i=n-2;i>=0;i--)
-    {
-        right[i] = min(right[i+1],nums[i]);
-    }
-    
-    for(int i=1;i<n;i++)
-    {
-        printf("%d %d\n",left[i-1],right[i]);
-        if(left[i-1]<=right[i])
-            return i;
-    }
-    
-    return n-1;
-
+    return ans+1;
 }
