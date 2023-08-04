300b7c340a71210039510bb732abf7cef2443b9c
Jitendra Singh <33808010+Bahubuli@users.noreply.github.com>
2023-08-04 12:31:48 +0530

    Time: 36 ms (86.54%), Space: 34.7 MB (32.25%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -17,10 +17,10 @@ public:
         
         if(root->val==val) return root;
         
-        if(root->val<val) return searchBST(root->right,val);
         if(root->val>val) return searchBST(root->left,val);
         
-        return NULL;
+        if(root->val<val) return searchBST(root->right,val);
         
+        return NULL;
     }
 };
