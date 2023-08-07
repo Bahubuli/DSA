f59bcdfbec643cebc8910cb98bc13e1a88e53d58
Jitendra Singh <33808010+Bahubuli@users.noreply.github.com>
2023-08-07 14:44:27 +0530

    Time: 604 ms (80.12%), Space: 235.9 MB (21.14%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -16,7 +16,6 @@ public:
             
             if(curr==d) return true;
             
-            
             for(int adj:graph[curr])
             {
                 if(!vis[adj])
@@ -26,6 +25,21 @@ public:
                 }
             }
         }
+        return false;
+    }
+    
+    bool dfs(vector<vector<int>>&graph,vector<bool>&vis,int s,int d)
+    {
+        if(s==d) return true;
+        
+        if(vis[s]) return false;
+        
+        vis[s] = true;
+        
+        for(int adj:graph[s])
+            if(dfs(graph,vis,adj,d))
+                return true;
+        
         return false;
     }
     
@@ -39,7 +53,11 @@ public:
             graph[e[1]].push_back(e[0]);
         }
         
-        return bfs(graph,s,d);
+       // return bfs(graph,s,d);
+        
+        vector<bool>vis(n,0);
+        
+        return dfs(graph,vis,s,d);
         
     }
 };
