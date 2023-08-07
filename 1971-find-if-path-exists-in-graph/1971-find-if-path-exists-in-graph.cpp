f7163b4a21a3c057f7fff5b6e78b19c97cc62204
Jitendra Singh <33808010+Bahubuli@users.noreply.github.com>
2023-08-07 11:55:51 +0530

    Time: 575 ms (81.88%), Space: 235.9 MB (20.11%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<int>>&edges,vector<bool>&vis,int s,int d)
+    bool dfs(vector<vector<int>>&graph,vector<bool>&vis,int s,int d)
     {
         if(s==d) return true;
         
@@ -11,56 +11,27 @@ public:
         
         bool ans = false;
         
-        for(int adj:edges[s])
-                ans = ans + dfs(edges,vis,adj,d); 
-
+        for(int adj:graph[s])
+        {
+            bool res = dfs(graph,vis,adj,d);
+            
+            ans = ans+res;
+        }
+        
         return ans;
     }
     
-    bool bfs(vector<vector<int>>&edges,int s,int d)
-    {
-        int n = edges.size();
-        vector<bool>vis(n,0);
-        queue<int>q;
+    bool validPath(int n, vector<vector<int>>& edges, int s, int d) {
         
-        q.push(s);
-        
-        while(q.size())
-        {
-            int curr = q.front();
-            q.pop();
-            
-            if(curr==d) return true;
-            
-            vis[curr] = true;
-            
-            for(int adj:edges[curr])
-            {
-                if(!vis[adj])
-                {
-                    q.push(adj);
-                    vis[adj] = true;
-                }
-            }
-            
-        }
-        return false;
-    }
-    
-    bool validPath(int n, vector<vector<int>>& edges, int s, int d) 
-    {
         vector<vector<int>>graph(n);
         
         for(auto e:edges)
-        {  
+        {
             graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);  
+            graph[e[1]].push_back(e[0]);
         }
         
-//         use either bfs or dfs 
-//         to check path
-           vector<bool>vis(n,0);
-           // return dfs(graph,vis,s,d);
-        return bfs(graph,s,d);
+        vector<bool>vis(n,0);
+        return dfs(graph,vis,s,d);
     }
 };
