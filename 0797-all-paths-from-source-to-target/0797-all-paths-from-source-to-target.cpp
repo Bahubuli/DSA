d51c23921f201467b6d0d058a170cbc641ca1bc6
Jitendra Singh <33808010+Bahubuli@users.noreply.github.com>
2023-08-07 14:59:42 +0530

    Time: 12 ms (71.77%), Space: 15.2 MB (29.86%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    
     void dfs(vector<vector<int>>&graph,int v,vector<int>path,vector<vector<int>>&paths)
     {
         path.push_back(v);
-        int n  = graph.size();
-        
-        if(v==n-1)
+        if(v==graph.size()-1)
             paths.push_back(path);
         
-        
-        for(auto adj:graph[v])
+        for(int adj:graph[v])
             dfs(graph,adj,path,paths);
-        
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph)
@@ -23,6 +18,5 @@ public:
         dfs(graph,0,path,paths);
         
         return paths;
-        
     }
 };
